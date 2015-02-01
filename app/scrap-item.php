57f263a613164839f04fb7bf087a4c37f1f99ac0
Diego Gonzalez <dfgonzalez@gmail.com>
2015-02-01 00:55:03 -0300

    Scrap Items function

--- app/scrap-item.php
+++ app/scrap-item.php
@@ -3,10 +3,20 @@
 require_once(__DIR__.'/config.php');
 require_once(__DIR__.'/../lib/meli.php');
 
-$meli = new Meli(MELI_APP_ID, MELI_SECRET);
+function scrapItems($items=array()) {
+	
+	$meli = new Meli(MELI_APP_ID, MELI_SECRET);
 
-$params = array();
-$result = $meli->get('/items?ids=MLA543411440,MLA543251257,MLA542374771', $params);
+	$params = array();
+	$params['ids'] = implode($items, ',');
+	$result = $meli->get('/items', $params);
 
-echo '<pre>';
-var_dump($result);
+	// echo '<pre>';
+	// var_dump($result['body']);
+	return $result['body'];
+	
+}
+
+// Testing
+$items = array('MLA543411440','MLA543251257','MLA542374771');
+scrapItems($items);
